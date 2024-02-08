05d07840f7b8162dc968ead1f19f1352c9e11c30
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2024-02-08 19:03:13 +0100

    after merge

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import LayoutContainer from '@/components/LayoutContainer.vue'
 import NavBar from "@/components/NavBar.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 import WelcomeMessage from "@/components/WelcomeMessage.vue";
 </script>
 <template>
-  <header class="max-w-[1000px] m-auto px-4">
-    <NavBar></NavBar>
-  </header>
-  <div class="max-w-[1000px] m-auto px-4">
+
+  <LayoutContainer>
+
+    <!-- Navbar here -->
+    <NavBar />
+    <header>
+      <nav class="flex items-center gap-4 py-2">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
+
+    <!-- Route view -->
+    <div class="bg-green-500 w-24 h-24 absolute left-1/2 top-28"></div>
     <RouterView />
-  </div>
+
+  </LayoutContainer>
 </template>
---
+++ src/components/LayoutContainer.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'LayoutContainer',
+    props: {
+        customClass: {
+            type: String,
+            required: false,
+        },
+    },
+    setup() {
+        const classes = "max-w-[1200px] mx-auto px-4 "
+        return { classes };
+    },
+});
+
+</script>
+
+<template>
+    <div :class="[classes, customClass]">
+        <slot></slot>
+    </div>
+</template>
