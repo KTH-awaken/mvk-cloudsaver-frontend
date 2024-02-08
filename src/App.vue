6fd4ad5279d1cfd8f0f37cd7cda91464d6b4476d
Hamada Aljarrah <hamadasepost@gmail.com>
2024-02-08 14:04:08 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="max-w-[1000px] m-auto px-4">
+    <nav class="flex items-center gap-4 py-2">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+  <div class="max-w-[1000px] m-auto px-4">
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<template>
+  <main>
+    <!-- Your code here -->
+    <HelloWorld message="Hello world component" />
+  </main>
+</template>
