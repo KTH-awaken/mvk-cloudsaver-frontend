c76f921692bd3095f909aff1162a5f9d46a82f18
Hamada Aljarrah <hamadasepost@gmail.com>
2024-02-29 12:31:56 +0100

    navbar and analysis

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,37 @@
-  <script>
-  import LayoutContainer from '@/components/LayoutContainer.vue'
+<script>
+import LayoutContainer from '@/components/LayoutContainer.vue'
 import { defineComponent } from 'vue'
 
-  export default defineComponent({
-    // components: {
-    //    LayoutContainer,
-    // },
-  
+export default defineComponent({
+  // components: {
+  //    LayoutContainer,
+  // },
+
 })
-  </script>
+</script>
 
 <template>
   <!-- <LayoutContainer> -->
 
-    <nav class="p-[70px] flex items-center justify-between py-2">
-      
-      <RouterLink to="/" class="flex items-center">
-        <img src="@/assets/CloudSaverLogo.png" alt="CloudSaver Logo" class="logo" width="70" height="70" >
-      </RouterLink>
-      
-      <div class="flex items-center gap-4">
-        <!-- <HomeButton></HomeButton> -->
-        <button class="text-primary-dark text-lg" id="signInButton">Sign In</button>
-      </div>
-    
-    </nav>
+  <nav class="p-[70px] flex items-center justify-between py-2">
+
+    <RouterLink to="/" class="flex items-center">
+      <img src="@/assets/CloudSaverLogo.png" alt="CloudSaver Logo" class="logo" width="70" height="70">
+    </RouterLink>
+
+    <div class="flex flex-row gap-4">
+      <RouterLink to="/overview" class="text-primary-dark text-lg">Overview</RouterLink>
+      <RouterLink to="/analysis" class="text-primary-dark text-lg">Analysis</RouterLink>
+      <RouterLink to="/Tutorial" class="text-primary-dark text-lg">Tutorial</RouterLink>
+    </div>
+
+    <div class="flex items-center gap-4">
+      <!-- <HomeButton></HomeButton> -->
+      <button class="text-primary-dark text-lg" id="signInButton">Sign In</button>
+    </div>
+
+  </nav>
   <!-- </LayoutContainer> -->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
