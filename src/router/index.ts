a9412b0b5f352e053da2b329f9a9d7db6d1a0a39
Hamada Aljarrah <hamadasepost@gmail.com>
2024-02-29 12:43:07 +0100

    clean up

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,11 +10,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/LandingPage.vue')
-    },
-    {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: HomeView
     },
     {
       path: '/about',
