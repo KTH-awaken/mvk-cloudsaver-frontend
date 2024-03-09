6896069f6ce82863150777ab397f9d77c62d946c
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2024-03-09 18:49:57 +0100

    fix

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,9 +1,6 @@
 
 <template>
     <div class="bg-accent full-size">
-        <!-- <FloatingBlob :imgSrc="GreenBlobImg" :blobWidth=800 :blobHeight=800 :interval=5000 :startX=1000 :startY=1000 :rotation=90 /> -->
-        <!-- <FloatingBlob :imgSrc="GreenBlobImg2" :blobWidth=1000 :blobHeight=1000 :interval=10000 :startX=0 :startY=0 :rotation=0 /> -->
-        <!-- <FloatingBlob :imgSrc="GreenBlobImg" :blobWidth=1000 :blobHeight=1000 :interval=10000 :startX=1000 :startY=0 :rotation=180 /> -->
         <TiltingCloud :imgSrc="GreenBlobImg4" :posX=-200 :posY=-200 :blobWidth=1130 :blobHeight=1000 :rotation=0 ></TiltingCloud>
         <TiltingCloud :imgSrc="GreenBlobImg" :posX="calculatePosition(60)" :posY=-400 :blobWidth=1300 :blobHeight=1000 :rotation=0 ></TiltingCloud>
         <TiltingCloud :imgSrc="GreenBlobImg" :posX="calculatePosition(60)" :posY=700 :blobWidth=1400 :blobHeight=1000 :rotation=0 ></TiltingCloud>
@@ -16,14 +13,12 @@ import GreenBlob2 from "@/assets/GreenBlob2.png"
 import GreenBlob3 from "@/assets/GreenBlob3.png"
 import GreenBlob4 from "@/assets/GreenBlob4.png"
 import TiltingCloud from "@/components/TiltingCloud.vue"
-// import FloatingBlob from "@/components/FloatingBlob.vue";
 
 
 export default {
     name: "BackgroundComponent",
     components: {
         TiltingCloud,
-        // FloatingBlob,
     },
     data() {
         return {
