430e5c6875b894a8192007d56cf148de26dfbe1e
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2024-03-09 18:45:49 +0100

    redy for deployment

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import LayoutContainer from '@/components/LayoutContainer.vue'
+import { RouterView } from 'vue-router'
+// import LayoutContainer from '@/components/LayoutContainer.vue'
 import NavBar from "@/components/NavBar.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
-import WelcomeMessage from "@/components/WelcomeMessage.vue";
+// import WelcomeMessage from "@/components/WelcomeMessage.vue";
 import Background from "@/components/Background.vue";
-import GlasCard from './components/GlasCard.vue';
+// import GlasCard from './components/GlasCard.vue';
 import { useUsageStore } from '@/stores/usage';
 const usageStore = useUsageStore()
 usageStore.fetchUsage()
